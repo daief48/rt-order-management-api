4df9da5cf27f62814125403123395fc4e38d650f
daief48 <daief20103048@gmail.com>
2025-12-03 20:58:33 +0600

    create login and register controller and route

---
+++ app/Http/Controllers/API/AuthController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request){
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        $token = $user->createToken('API Token')->accessToken;
+
+        return response()->json(['token' => $token], 201);
+    }
+
+    public function login(Request $request){
+        $user = User::where('email', $request->email)->first();
+
+        if(!$user || !Hash::check($request->password, $user->password)){
+            return response()->json(['error'=>'Invalid Credentials'], 401);
+        }
+
+        $token = $user->createToken('API Token')->accessToken;
+
+        return response()->json(['token' => $token], 200);
+    }
+}
